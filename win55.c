3cc58def2041a3c4fc901e22154c44884b6dc887
allwin <36722666+allwin13makimai@users.noreply.github.com>
2018-08-01 15:51:25 +0530

    Create win55.c

---
+++ win55.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+char a[100],b[100];
+int n,i,j,z;
+scanf("%s",&a);
+for(i=0;a[i]!='\0';i++)
+{
+j=0;
+}
+for(j=0;j<i;j++)
+printf("%c",a[j]+2);
+scanf("%s",&b);
+printf(" ");
+for(z=0;b[z]!='\0';z++)
+{
+j=0;
+}
+for(j=0;j<z;j++)
+printf("%c",b[j]+2);
+}
