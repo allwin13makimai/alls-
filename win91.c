aefcea0c92b7149c18e8d566b11bd69f2aed8693
allwin <36722666+allwin13makimai@users.noreply.github.com>
2019-01-07 11:46:33 +0530

    Create win91.c

---
+++ win91.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char t[30];
+char k;
+int j;
+clrscr();
+scanf("%s",t);
+for(j=0;t[j]!=NULL;j++)
+{
+if(t[j]==k)
+{
+printf("the position is:%d",j);
+}
+}
+getch();
+}
